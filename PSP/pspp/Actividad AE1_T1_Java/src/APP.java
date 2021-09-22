dc82953cb0bfa3830cc8eebb37ce2b633e00c32d
PORTATILNELA\DAM2 <reneribera3@gmail.com>
2021-09-22 11:08:03 +0200

    Finalitzacio de la activitat 1

--- PSP/pspp/Actividad AE1_T1_Java/src/APP.java
+++ PSP/pspp/Actividad AE1_T1_Java/src/APP.java
@@ -2,7 +2,16 @@
 public class APP {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// declaracio del metode en el main
+		sayHello();
+
+	}
+
+	// creacio del metode sayHello que escriu hola mundo per consola
+	public static void sayHello() {
+
+		System.out.println("Hola mundo");
 
 	}
 
